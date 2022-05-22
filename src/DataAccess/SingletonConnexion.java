733a08d7b1fc98514158cbd34d34a76d0a8fa04e
Bollemii <100069388+Bollemii@users.noreply.github.com>
2022-05-22 10:31:53 +0200

    Update SingletonConnexion.java

--- src/DataAccess/SingletonConnexion.java
+++ src/DataAccess/SingletonConnexion.java
@@ -9,15 +9,10 @@ public class SingletonConnexion {
 
 	private SingletonConnexion() throws SQLException {
 		connection = DriverManager.getConnection(
-				"jdbc:mysql://DESKTOP-G9LT55S:3306/java_project",
-				"bolle",
-				"1111"
-		);
-		/*connection = DriverManager.getConnection(
 				"jdbc:mysql://127.0.0.1:3306/java_project",
 				"root",
 				"root"
-		);*/
+		);
 	}
 
 	public static Connection getInstance() throws DataException {
